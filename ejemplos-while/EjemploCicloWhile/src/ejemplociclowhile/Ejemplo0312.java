9996ad6339735a2090524e05471bfb3ef801c432
Emilio J Cueva <ecueva611@gmail.com>
2024-11-20 12:31:12 -0500

    Tercer Cambio

---
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo0312.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplociclowhile;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo0312 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+        int limite = 5;
+        int contador = 1;
+        double suma = 0;/*declaramos a suma como double para que el promedio no
+        afecte y sea double*/
+        int valor_ingresado;
+        double promedio;
+        
+        while (contador <= limite) { 
+            System.out.println("Ingrese el valor a sumar");
+            valor_ingresado = entrada.nextInt();
+            suma = suma + valor_ingresado;
+            System.out.printf("Valor ingresado %d\n", valor_ingresado);
+            contador = contador + 1;
+        }
+        promedio = suma / limite;
+        System.out.printf("La suma final es %2f\n", suma);
+        System.out.printf("EL promedio final es %.2f\n", promedio);
+
+    }
+}
